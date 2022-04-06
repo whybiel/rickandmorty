b061bcdc7ba3cfbbaddb47ed980aed4668d22ec0
gabrielvnw <gabrimoraes2004@gmail.com>
2022-04-06 15:51:18 -0300

    complementei o estilo

--- src/services/apirick.js
+++ src/services/apirick.js
@@ -1,19 +1,37 @@
 import React from "react";
 import axios from "axios";
 import styled from "styled-components";
-import Carousel from ""
+import Carousel from "react-elastic-carousel"
 
 const Box = styled.div`
     width:100%;
+    height:100vh;
     display:flex;
     align-items:center;
+    justify-content:center;
     flex-direction:column;
+    background-size:100% 100vh;
+    background-image:url('https://a-static.besthdwallpaper.com/papel-de-parede-da-dimensao-de-deus-de-rick-e-morty-papel-de-parede-1600x900-57412_47.jpg');
 `
-
+const Title = styled.h1`
+    width:100%;
+    font-size:3rem;
+    text-align:center;
+    margin:0 0 3vh 0;
+    font-family: 'Creepster', cursive;
+    background-image: linear-gradient(to bottom, #008B00 0%, #7CFC00 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;   
+`
+const Carrossel = styled(Carousel)`
+    width:100%;
+    height:70vh;
+`
 const Container = styled.div`
-    width:50vw;
-    height:35vh;
-    background-color:red;
+    width:80vw;
+    height:60vh;
+    background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
     margin:5vh 0 0 0;
     display:flex;
     align-items:center;
@@ -34,26 +52,37 @@ const BoxTexts = styled.div`
 const Image = styled.img`  
     width:100%;
     height:100%;
-    border-radius:20px 0px 0px 20px;
+    border-radius:20px 0 0 20px;
 `
 
 const Name = styled.h2`
     width:100%;
+    font-size:3rem;
     text-align:center;
     margin:0 0 3vh 0;
+    font-family: 'Creepster', cursive;
+    background-image: linear-gradient(to bottom, #008B00 0%, #7CFC00 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
 `
 
 const Status = styled.h3`
     width:100%;
+    font-size:2rem;
     text-align:center;
-    margin:0 0 3vh 0;   
+    margin:0 0 3vh 0;
+    font-family: 'Caveat Brush', cursive; 
+    color:#fff;  
 `
 
-const Location = styled.h4``
-
-const Ball = styled.div`
-    width:5vw;
-    background:#000;
+const Location = styled.h4`
+    width:100%;
+    font-size:1.5rem;
+    text-align:center;
+    margin:0 0 3vh 0;
+    font-family: 'Caveat Brush', cursive; 
+    color:#fff;  
 `
 
 
@@ -89,7 +118,8 @@ export default class Morty extends React.Component {
     render() {
         return (
             <Box>
-                <h1>Personagens de Rick and Morty</h1>
+                <Title>Personagens de Rick and Morty</Title>
+                <Carrossel>
                 {this.state.persons.map((item) => (
                     <Container key={item.id}>
                         <SubContainer>
@@ -101,7 +131,9 @@ export default class Morty extends React.Component {
                             <Location>{item.location.name}</Location>
                         </BoxTexts>
                     </Container>
+                    
                 ))}
+                </Carrossel>
             </Box>
         )
     }
